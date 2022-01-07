13b16eb1cd8893963ae5a62d2c2437338d98dd5d
TgogoHypercubed <tgogo.gold@gmail.com>
2022-01-06 22:30:17 -0800

    2230 010622

--- src/main/java/com/hyperchatacan/testMod1/testMod1.java
+++ src/main/java/com/hyperchatacan/testMod1/testMod1.java
@@ -19,12 +19,12 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
-public class ExampleMod
+public class testMod1
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public testMod1() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
