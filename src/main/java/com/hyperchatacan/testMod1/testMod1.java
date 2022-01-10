8b3e4da2203d01af274c4341cdb6e1750fdeb856
TgogoHypercubed <tgogo.gold@gmail.com>
2022-01-09 19:13:16 -0800

    Co-authored-by: cedricttChartier <requiem85@users.noreply.github.com>

--- src/main/java/com/hyperchatacan/testMod1/testMod1.java
+++ src/main/java/com/hyperchatacan/testMod1/testMod1.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("testmod1")
 public class testMod1
 {
     // Directly reference a log4j logger.
@@ -47,7 +47,7 @@ public class testMod1
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("testmod1", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
